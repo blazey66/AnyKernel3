bd096b69a02985521366aff8ac959d25ed31fbf3
blaze <blazeyhere@gmail.com>
2025-06-18 21:06:04 +0530

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=4.19 kernel with rksu-susfs
+kernel.string=4.19 kernel with KernelSU-next
 do.devicecheck=1
 do.modules=0
 do.systemless=1
